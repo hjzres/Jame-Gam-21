596a310a7c0d23223dd8bc7aa7724dd6ece8cbaf
hjzre <142443853+hjzres@users.noreply.github.com>
2023-09-01 14:32:15 -0400

    Added storage and agility to the static data with their starting numbers

--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class StaticData : MonoBehaviour
 {
     public static int money;
+    public static int storage;
+    public static int agility;
+
+    private void Awake()
+    {
+        money = 0;
+        storage = 12;
+        agility = 1;
+    }
 }
