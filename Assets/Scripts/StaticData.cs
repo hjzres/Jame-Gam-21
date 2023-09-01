fadb90b8aca6b2ac4d22b71a83cd567c956b89f8
hjzre <142443853+hjzres@users.noreply.github.com>
2023-09-01 15:05:50 -0400

    Update StaticData.cs

--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class StaticData : MonoBehaviour
 {
     // These are the things that never will reset
-    public static int money = 10; 
+    public static int money = 0; 
     public static int storage = 12;
     public static int agility = 1;
 }
